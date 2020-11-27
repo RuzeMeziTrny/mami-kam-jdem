531a215e87d5838c3aa647d0b7909c55074e8313
Pavlina Baldova <pavlina.bal@seznam.cz>
2020-11-27 12:54:45 +0100

    font categoryList

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -28,6 +28,30 @@ const restauraceKavarny = [
       address: 'Lochotínská 656, 109 00 Praha 15 - Horní Měcholupy',
       web: 'https://www.pivovar-hostivar.cz',
     },
+    {
+      image: "../../assets/images/restaurants/bb.png",
+      name: 'Bageterie Boulevard',
+      address: 'Hornoměcholupská 764, 10900 Praha 15 - Horní Měcholupy',
+      web: 'http://www.bb.cz',
+    },
+    {
+      image: "../../assets/icons/restaurants.svg",
+      name: 'Cyklobar Ve Statku',
+      address: 'Ke Štítu 33, 104 00 Praha - Křeslice',
+      web: "",
+    },
+    {
+      image: "../../assets/images/restaurants/dolce-carosello.png",
+      name: 'Dolce Carosello',
+      address: 'Turnovského 497/2, 100 00 Praha 10 - Strašnice',
+      web: 'https://www.dolce-carosello.cz',
+    },
+    {
+      image: "../../assets/images/restaurants/hostivar.png",
+      name: 'Hostivar 1',
+      address: 'Lochotínská 656, 109 00 Praha 15 - Horní Měcholupy',
+      web: 'https://www.pivovar-hostivar.cz',
+    },
   ]
   
 
