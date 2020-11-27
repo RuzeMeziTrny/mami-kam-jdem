22413c03b962755f877fc1eed3d45ff825547fc3
Pavlina Baldova <pavlina.bal@seznam.cz>
2020-11-27 16:38:47 +0100

    snaha o PlaygroundsPage

--- src/components/CategoryItemPlayground/index.jsx
+++ src/components/CategoryItemPlayground/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import './styles.css';
+import { useParamas} from "react-router-dom"
+
+
+/*const playgroundsRoute = [
+  { path: '/Playgrounds/:playgroundsPage", component: PlaygroundsPage }
+]*/
+
+
 
 export const CategoryItemPlayground = (props) => {
   return (
@@ -12,6 +20,10 @@ export const CategoryItemPlayground = (props) => {
       <div className="category-item__details">
         <p className="category-item__name">{props.name}</p>
         <p className="category-item__address">{props.address}</p>
+        
+       {/* <Link to="/hriste" className= "category-item__web" >
+            Dětská hřiště */}
+        
         <a className="category-item__web" href={props.web}>
           Podrobnosti o hřišti
         </a>
