132161812b6bb1f160bc69a33eb8ec18d74dae9f
Pavlina Baldova <pavlina.bal@seznam.cz>
2020-11-27 15:36:30 +0100

    arrows for tablet

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -7,22 +7,22 @@ const restauraceKavarny = [
     {
       image: "../../assets/images/restaurants/bb.png",
       name: 'Bageterie Boulevard',
-      address: 'Hornoměcholupská 764, 10900 Praha 15 - Horní Měcholupy',
+      address: 'Hornoměcholupská 764,Praha 15',
       web: 'http://www.bb.cz',
     },
     {
       image: "../../assets/icons/restaurants.svg",
       name: 'Cyklobar Ve Statku',
-      address: 'Ke Štítu 33, 104 00 Praha - Křeslice',
+      address: 'Ke Štítu 33, Praha 114',
       web: "",
     },
-    {
+   /* {
       image: "../../assets/images/restaurants/dolce-carosello.png",
       name: 'Dolce Carosello',
       address: 'Turnovského 497/2, 100 00 Praha 10 - Strašnice',
       web: 'https://www.dolce-carosello.cz',
     },
-    {
+   {
       image: "../../assets/images/restaurants/hostivar.png",
       name: 'Hostivar 1',
       address: 'Lochotínská 656, 109 00 Praha 15 - Horní Měcholupy',
@@ -51,13 +51,16 @@ const restauraceKavarny = [
       name: 'Hostivar 1',
       address: 'Lochotínská 656, 109 00 Praha 15 - Horní Měcholupy',
       web: 'https://www.pivovar-hostivar.cz',
-    },
+    },*/
   ]
   
 
 export const CategoryList = () => {
     return (
+     
+      
       <div className="category-list">
+      <img className="category-arrow category-arrow--left" src="../../assets/icons/arrow_left.svg" alt="šipka doleva"/>
               { restauraceKavarny.map((data) => (
      <CategoryItemGeneral
      image = {data.image}
@@ -66,7 +69,10 @@ export const CategoryList = () => {
      web = {data.web}
      />
      ))}
+   <img className="category-arrow category-arrow--right" src="../../assets/icons/arrow_right.svg" alt="šipka doprava"/>
       </div>
+     
+    
     );
   };
   
