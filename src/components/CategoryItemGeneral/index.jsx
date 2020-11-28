ce6bacc831803db4e045077f659b8768a6c26fa8
PavlinaBaldova <pavlina.bal@seznam.cz>
2020-11-28 10:46:16 +0100

    scrollList

--- src/components/CategoryItemGeneral/index.jsx
+++ src/components/CategoryItemGeneral/index.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import './styles.css';
 
-
-
 export const CategoryItemGeneral = (props) => {
   const colonIndex = props.web.indexOf(':');
 
   return (
-    <div className="category-item">
-      <img
-        className="category-item__image"
-        src={props.image}
-        alt="obrázek místa"
-      />
-      <div className="category-item__details">
-        <p className="category-item__name">{props.name}</p>
-        <p className="category-item__address">{props.address}</p>
-        <a className="category-item__web" href={props.web} target="_blank">
-          {props.web.slice(colonIndex + 3, props.web.length)}
-        </a>
+    <div className="category-container">
+      <div className="category-item">
+        <img
+          className="category-item__image"
+          src={props.image}
+          alt="obrázek místa"
+        />
+        <div className="category-item__details">
+          <p className="category-item__name">{props.name}</p>
+          <p className="category-item__address">{props.address}</p>
+          <a className="category-item__web" href={props.web} target="_blank">
+            {props.web.slice(colonIndex + 3, props.web.length)}
+          </a>
+        </div>
       </div>
     </div>
   );
 };
-
-
--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -15,52 +15,52 @@ const restauraceKavarny = [
     address: 'Ke Štítu 33, Praha 114',
     web: '',
   },
-  /* {
-     image: "../../assets/images/restaurants/dolce-carosello.png",
-     name: 'Dolce Carosello',
-     address: 'Turnovského 497/2, 100 00 Praha 10 - Strašnice',
-     web: 'https://www.dolce-carosello.cz',
-   },
   {
-     image: "../../assets/images/restaurants/hostivar.png",
-     name: 'Hostivar 1',
-     address: 'Lochotínská 656, 109 00 Praha 15 - Horní Měcholupy',
-     web: 'https://www.pivovar-hostivar.cz',
-   },
-   {
-     image: "../../assets/images/restaurants/bb.png",
-     name: 'Bageterie Boulevard',
-     address: 'Hornoměcholupská 764, 10900 Praha 15 - Horní Měcholupy',
-     web: 'http://www.bb.cz',
-   },
-   {
-     image: "../../assets/icons/restaurants.svg",
-     name: 'Cyklobar Ve Statku',
-     address: 'Ke Štítu 33, 104 00 Praha - Křeslice',
-     web: "",
-   },
-   {
-     image: "../../assets/images/restaurants/dolce-carosello.png",
-     name: 'Dolce Carosello',
-     address: 'Turnovského 497/2, 100 00 Praha 10 - Strašnice',
-     web: 'https://www.dolce-carosello.cz',
-   },
-   {
-     image: "../../assets/images/restaurants/hostivar.png",
-     name: 'Hostivar 1',
-     address: 'Lochotínská 656, 109 00 Praha 15 - Horní Měcholupy',
-     web: 'https://www.pivovar-hostivar.cz',
-   },*/
+    image: '../../assets/images/restaurants/dolce-carosello.png',
+    name: 'Dolce Carosello',
+    address: 'Turnovského 497/2, Praha 10',
+    web: 'https://www.dolce-carosello.cz',
+  },
+  {
+    image: '../../assets/images/restaurants/hostivar.png',
+    name: 'Hostivar 1',
+    address: 'Lochotínská 656, Praha 15',
+    web: 'https://www.pivovar-hostivar.cz',
+  },
+  {
+    image: '../../assets/images/restaurants/bb.png',
+    name: 'Bageterie Boulevard',
+    address: 'Hornoměcholupská 764, Praha 15',
+    web: 'http://www.bb.cz',
+  },
+  {
+    image: '../../assets/icons/restaurants.svg',
+    name: 'Cyklobar Ve Statku',
+    address: 'Ke Štítu 33, Praha',
+    web: '',
+  },
+  {
+    image: '../../assets/images/restaurants/dolce-carosello.png',
+    name: 'Dolce Carosello',
+    address: 'Turnovského 497/2, Praha 10',
+    web: 'https://www.dolce-carosello.cz',
+  },
+  {
+    image: '../../assets/images/restaurants/hostivar.png',
+    name: 'Hostivar 1',
+    address: 'Lochotínská 656, Praha 15',
+    web: 'https://www.pivovar-hostivar.cz',
+  },
 ];
 
 export const CategoryList = () => {
   return (
     <div className="category-list">
-      <img
+      {/* <img
         className="category-arrow category-arrow--left"
         src="../../assets/icons/arrow_left.svg"
         alt="šipka doleva"
-      />
+     /> */}
       {restauraceKavarny.map((data) => (
         <CategoryItemGeneral
           key={data.name}
@@ -70,11 +70,11 @@ export const CategoryList = () => {
           web={data.web}
         />
       ))}
-      <img
+      {/* <img
         className="category-arrow category-arrow--right"
         src="../../assets/icons/arrow_right.svg"
         alt="šipka doprava"
-      />
+     />*/}
     </div>
   );
 };
