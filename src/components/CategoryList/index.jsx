ad7f6af564e33a625fc767236ef7565df58aef5b
Ivana Ruzickova <shopping@mezitrny.cz>
2020-11-28 19:07:44 +0100

    CategoryList switch

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import { Switch, useLocation } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { CategoryItemGeneral } from '../CategoryItemGeneral';
 import { CategoryItemPlayground } from '../CategoryItemPlayground';
 import './styles.css';
 
 export const CategoryList = (props) => {
-  const location = useLocation();
-
-  /*
-<Switch>
-<Route path="/hriste" render={() => pole category item plg</>}
-<Route render={() => pole category item general</>}
-</Switch>
-*/
-
   return (
     <div className="category-list">
-      {location.pathname === '/hriste' /*startsWith('/hriste')*/
-        ? props.array.map((place) => (
-            <CategoryItemPlayground
-              key={place.id}
-              image={place.image}
-              name={place.name}
-              address={place.address}
-              id={place.id}
-            />
-          ))
-        : props.array.map((place) => (
-            <CategoryItemGeneral
-              key={place.id}
-              image={place.image}
-              name={place.name}
-              address={place.address}
-              web={place.web}
-            />
-          ))}
+      <Switch>
+        <Route
+          path="/hriste"
+          render={() =>
+            props.array.map((place) => (
+              <CategoryItemPlayground
+                key={place.id}
+                image={place.image}
+                name={place.name}
+                address={place.address}
+                id={place.id}
+              />
+            ))
+          }
+        />
+        <Route
+          render={() =>
+            props.array.map((place) => (
+              <CategoryItemGeneral
+                key={place.id}
+                image={place.image}
+                name={place.name}
+                address={place.address}
+                web={place.web}
+              />
+            ))
+          }
+        />
+      </Switch>
     </div>
   );
 };
